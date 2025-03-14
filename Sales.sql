c0de22035cb81cee34925d3a0bac203f92680b9b
avdhut.ssh <avdhut.ssh@gmail.com>
2025-03-14 21:46:13 +0530

    Create Table

--- Sales.sql
+++ Sales.sql
@@ -6,3 +6,12 @@ CREATE SCHEMA IF NOT EXISTS Sales;
 ### Start Using Database
 USE sales;
 
+### Create Table
+CREATE TABLE sales
+(
+	purchase_number INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date_of_purchase DATE NOT NULL,
+    customer_id INT,
+    item_code VARCHAR(10) NOT NULL
+);
+
