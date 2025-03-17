0f501e1ac908394c9c610a45cc1a08aa155f3260
avdhut.ssh <avdhut.ssh@gmail.com>
2025-03-17 20:11:33 +0530

    Unique Key

--- Sales.sql
+++ Sales.sql
@@ -124,3 +124,22 @@ CREATE TABLE orders
     order_currency VARCHAR(10),
     fOREIGN KEY (order_info_id) REFERENCES order_info(order_info_id)
 );
+
+###Unique Key
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email_address VARCHAR(255),
+    number_of_complaints INT,
+	PRIMARY KEY (customer_id)
+);
+
+ALTER TABLE customers
+ADD COLUMN gender ENUM('M', 'F') AFTER last_name;
+
+INSERT INTO customers (first_name, last_name, gender, email_address, number_of_complaints)
+VALUES ('John', 'Mackinley', 'M', 'john.mckinley@365careers.com', 0);
+
+ALTER TABLE customers
+ADD UNIQUE KEY (email_address);
