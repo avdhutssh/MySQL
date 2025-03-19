46f3abf9fbcd4f562f86364e17eb93a9d5091dfb
avdhut.ssh <avdhut.ssh@gmail.com>
2025-03-19 11:42:34 +0530

    OR

--- Queries.sql
+++ Queries.sql
@@ -238,3 +238,7 @@ WHERE first_name = 'Elvis';
 ###AND
 SELECT * FROM employees
 WHERE first_name = 'Kellie' AND gender = 'F';
+
+###OR
+SELECT * FROM employees
+WHERE first_name = 'Kellie' OR first_name = 'Aruna';
